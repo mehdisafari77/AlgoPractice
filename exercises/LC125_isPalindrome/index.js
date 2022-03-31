0711d28b60af4897d04dad67fa6c537c44896442
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-30 20:49:32 -0700

    add basic code

---
+++ exercises/LC125_isPalindrome/index.js
@@ -0,0 +1,19 @@
+// Link: https://leetcode.com/problems/valid-palindrome/
+
+function isPalindrome(s) {
+    s = s.toLowerCase().replace(/[\W_]/g, "");
+
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        if(s[left] !== s[right]) {
+            return false
+        }
+        left++
+        right--
+    }
+    return true
+}
+
+module.exports = isPalindrome;
