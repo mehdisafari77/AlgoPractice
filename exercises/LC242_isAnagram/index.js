7d9a62d97526d629534b7b4358841c20d6128f43
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-01 19:54:44 -0700

    add two new algos

--- exercises/LC242_isAnagram/index.js
+++ exercises/LC242_isAnagram/index.js
@@ -1,3 +1,7 @@
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+//Link: https://leetcode.com/problems/valid-anagram/
+
 function isAnagram(s, t) {}
 
 module.exports = isAnagram;
--- exercises/LC3_lengthOfLongestSubstring/index.js
+++ exercises/LC3_lengthOfLongestSubstring/index.js
@@ -4,6 +4,27 @@
 // lengthOfLongestSubstring("abcabcbb") --> 3 since length of "abc"
 // lengthOfLongestSubstring("bbbbb") --> 1 since length of "b"
 
-function lengthOfLongestSubstring(s) {}
+//Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+//Sliding window approach
+
+function lengthOfLongestSubstring(s) {
+    let windowCharsMap = {};
+    let windowStart = 0;
+    let maxLength = 0;
+
+    for ( let i = 0; i < s.length; i++) {
+        const endChar = s[i];
+
+        if (windowCharsMap[endChar] >= windowStart) {
+            windowStart = windowCharsMap[endChar] + 1
+        }
+
+        windowCharsMap[endChar] = i;
+        maxLength = Math.max(maxLength, i - windowStart + 1);
+    }
+
+    return maxLength
+}
 
 module.exports = lengthOfLongestSubstring;
