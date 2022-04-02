51cf9d906bb28ec5593cd9ccb9a8223eba961000
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-01 20:00:44 -0700

    add new algo

--- exercises/LC242_isAnagram/index.js
+++ exercises/LC242_isAnagram/index.js
@@ -2,6 +2,30 @@
 
 //Link: https://leetcode.com/problems/valid-anagram/
 
-function isAnagram(s, t) {}
+function isAnagram(s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    const sCharCounts = {}; // {H: 1. E: 1, L: 2, 0: 1}
+
+    //Fill sCharCounts
+    for (let i = 0; i < s.length; i++) {
+        const sChar = s[i];
+        sCharCounts[sChar] = sCharCounts[sChar] + 1 || 1;
+    }
+
+    for (let i = 0; i < t.length; i++) {
+        const tChar = t[i];
+
+        if (!sCharCounts[tChar]) {
+            return false;
+        } else {
+            sCharCounts[tChar]--
+        }
+    }
+
+    return true
+}
 
 module.exports = isAnagram;
--- exercises/LC49_groupAnagrams/index.js
+++ exercises/LC49_groupAnagrams/index.js
@@ -1,3 +1,8 @@
+
+// Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+//Link: https://leetcode.com/problems/group-anagrams/
+
 function groupAnagrams(strs) {}
 
 module.exports = groupAnagrams;
