27e96f34b8963cb4f9e9f2b588a56bb17300f90a
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-01 20:41:19 -0700

    add new algo

--- exercises/LC20_isValid/index.js
+++ exercises/LC20_isValid/index.js
@@ -1,3 +1,25 @@
-const isValid = s => {};
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// Time Complexity: O(n) We iterate through input just once
+
+// Space Complexity: O(n) In worst case, stack is same length as input string
+
+
+const isValid = s => {
+    let stack = [];
+    let pairsHashMap = { "(": ")", "{": "}", "[": "]" };
+
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+
+        if (pairsHashMap[char]) {
+            stack.push(char);
+        } else if (pairsHashMap[stack.pop()] !== char) {
+            return false;
+        }
+    }
+
+    return stack.length === 0
+};
 
 module.exports = isValid;
