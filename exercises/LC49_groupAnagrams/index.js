c7d6c3cdfde609546db18daa8703ec07ae550395
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-01 20:05:55 -0700

    add new algo

--- exercises/LC49_groupAnagrams/index.js
+++ exercises/LC49_groupAnagrams/index.js
@@ -3,6 +3,21 @@
 
 //Link: https://leetcode.com/problems/group-anagrams/
 
-function groupAnagrams(strs) {}
+function groupAnagrams(strs) {
+    let grouped = {};
+
+    for (let i = 0; i < strs.length; i++) {
+        const word = strs[i];
+        const key = word.split('').sort().join('');
+
+        if (!grouped[key]) {
+            grouped[key] = []
+        };
+
+        grouped[key].push(word);
+    }
+
+    return Object.values(grouped);
+}
 
 module.exports = groupAnagrams;
