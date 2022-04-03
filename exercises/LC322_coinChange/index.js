3b2716bd20f0de7257800b9b0431cac6ab2cd874
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-02 22:37:35 -0700

    add new algo

--- exercises/LC322_coinChange/index.js
+++ exercises/LC322_coinChange/index.js
@@ -1,12 +1,11 @@
-
 /* You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money. Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1. You may assume that you have an infinite number of each kind of coin. */
 
 
 // Link: https://leetcode.com/problems/coin-change/
 
-// Time Complexity: 
+// Time Complexity: O(A * C) A = amount and C = # of coins
 
-// Space Complexity: 
+// Space Complexity: O(A)
 
 function coinChange(coins, amount) {
     let dpMinCoins = new Array(amount + 1).fill(Infinity);
--- exercises/LC62_uniquePaths/index.js
+++ exercises/LC62_uniquePaths/index.js
@@ -1,3 +1,38 @@
-function uniquePaths(m, n) {}
+/* There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+The test cases are generated so that the answer will be less than or equal to 2 * 109. */
+
+
+// Link: https://leetcode.com/problems/unique-paths/
+
+// Time Complexity: O(N * M)
+
+// Space Complexity: O(N * M)
+
+
+function uniquePaths(m, n) {
+    const dpMatrix = [];
+
+    for (let row = 1; row <= n; row++) {
+        dpMatrix.push([]);
+    }
+
+    for (let row = 0; row < n; row++) {
+        dpMatrix[row][0] = 1;
+    }
+
+    for (let col = 0; col < m; col++) {
+        dpMatrix[0][col] = 1;
+    }
+
+    for (let row = 1; row < n; row++) {
+        for (let col = 1; col < m; col++) {
+            dpMatrix[row][col] = dpMatrix[row][col - 1] + dpMatrix[row - 1][col]
+        }
+    }
+    return dpMatrix[dpMatrix.length -1][m -1]
+}
 
 module.exports = uniquePaths;
