6b0f45c9e199de1d4dc27dd015553026641db052
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-02 22:43:24 -0700

    new algo added

--- exercises/LC217_containsDuplicate/index.js
+++ exercises/LC217_containsDuplicate/index.js
@@ -1,3 +1,25 @@
-const containsDuplicate = nums => {};
+/* Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct. */
+
+// Link: https://leetcode.com/problems/contains-duplicate/
+
+// Time Complexity: O(N * M)
+
+// Space Complexity: O(N * M)
+
+const containsDuplicate = nums => {
+    const visitedNums = {} // {7: true, 2: true} 
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+
+        if (visitedNums[num]) {
+            return true;
+        } else {
+            visitedNums[num] = true;
+        }
+    }
+
+    return false;
+};
 
 module.exports = containsDuplicate;
