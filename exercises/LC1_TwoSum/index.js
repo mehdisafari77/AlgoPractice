c09dcf3ef32c3d137a14a65f7cbb73da78319847
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-04 19:52:24 -0700

    new algo added

--- exercises/LC1_TwoSum/index.js
+++ exercises/LC1_TwoSum/index.js
@@ -1,11 +1,30 @@
-// Given an array of integers, return an array of indices of the two numbers
-// that add up to the target.
-// You may assume that each input would have exactly one solution, and
-// you may not use the same element twice.
-// --- Examples
-// twoSum([2,7,11,15], 9) --> [0,1]
-// twoSum([3,5,4], 9) --> [1,2]
+/* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-function twoSum(arr, target) {}
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order. */
+
+// Link: https://leetcode.com/problems/two-sum/
+
+// Time Complexity: O(n)
+
+// Space Complexity: O(n)
+
+function twoSum(arr, target) {
+    const numsVisited = {};
+    const res = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        const num = arr[i];
+        const complement = target - num;
+
+        if (numsVisited[complement] !== undefined) {
+            res.push(i);
+            res.push(numsVisited[complement]);
+        }
+        numsVisited[num] = i;
+    }
+    return res;
+}
 
 module.exports = twoSum;
