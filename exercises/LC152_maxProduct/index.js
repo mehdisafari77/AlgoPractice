003029b0ff7a55082a6e4c4a8849b6ebb79b8720
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-04 20:11:16 -0700

    new algo

--- exercises/LC152_maxProduct/index.js
+++ exercises/LC152_maxProduct/index.js
@@ -1,3 +1,36 @@
-function maxProduct(nums) {}
+/* Given an integer array nums, find a contiguous non-empty subarray within the array that has the largest product, and return the product.
 
+The test cases are generated so that the answer will fit in a 32-bit integer.
+
+A subarray is a contiguous subsequence of the array. */
+
+// Link: https://leetcode.com/problems/maximum-product-subarray/
+
+// Time Complexity: O(n)
+
+// Space Complexity: O(1)
+
+function maxProduct(nums){
+    let maxTillIndex = [nums[0]];
+    let minTillIndex = [nums[0]];
+    let max = nums [0];
+
+    for (let i = 1; i < nums. length; i++) {
+        const num = nums[i];
+        maxTillIndex[i] = Math.max(
+            num,
+            num * maxTillIndex[i - 1],
+            num * minTillIndex[i - 1]
+        );
+
+        minTillIndex[i] = Math.min(
+            num,
+            num * maxTillIndex[i - 1],
+            num * minTillIndex[i - 1]
+        );
+
+        max = Math.max(max, maxTillIndex[i]);
+    }
+    return max;
+}
 module.exports = maxProduct;
