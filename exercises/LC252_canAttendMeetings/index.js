a9b9952c9bb736d055baa4eb712dde07cd57edf5
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-04 20:42:17 -0700

    add new algo

--- exercises/LC252_canAttendMeetings/index.js
+++ exercises/LC252_canAttendMeetings/index.js
@@ -1,9 +1,29 @@
-// Given an array of meeting time intervals consisting of start and end
-// times [s1, e1], [s2, e2], ... , determine if a person could attend all meetings.
-// ---------------------------
-// canAttendMeetings([[0, 30], [5, 10], [15, 20]]) --> false
-// canAttendMeetings([[7, 10], [2, 4]]) --> true
+/* Given an array of meeting time intervals where intervals[i] = [starti, endi], determine if a person could attend all meetings. */
 
-const canAttendMeetings = intervals => {};
+// Link: https://leetcode.com/problems/meeting-rooms/
+
+// Time Complexity: O(log n)
+
+// Space Complexity: O(1)
+
+const canAttendMeetings = intervals => {
+    const starts = [];
+    const ends = [];
+
+    for (let i = 0; i < intervals.length; i++) {
+        const subarray = intervals[i];
+        starts.push(subarray[0]);
+        ends.push(subarray[1]);
+    }
+
+    starts.sort((a,b) => a - b);
+    ends.sort((a,b) => a - b);
+
+    for (let i = 0; i < starts.length; i++) {
+        if (starts[i + 1] < ends[i]) return false;
+    }
+
+    return true
+};
 
 module.exports = canAttendMeetings;
