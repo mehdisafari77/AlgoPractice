6947439892d9b08fcea0aab98400ba28dd434314
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-08 13:56:32 -0700

    add new algo

--- exercises/LC200_numIslands/index.js
+++ exercises/LC200_numIslands/index.js
@@ -1,3 +1,13 @@
+/* Given the head of a singly linked list, reverse the list, and return the reversed list. */
+
+// Link: https://leetcode.com/problems/merge-two-sorted-lists/
+
+// Solution 1: While Loop
+
+// Time Complexity: O(N)
+
+// Space Complexity: O(1)
+
 function numIslands(grid) {}
 
 module.exports = numIslands;
