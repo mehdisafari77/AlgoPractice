73f0420c7ed9507f3548694ce2755d85b1f76499
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-08 14:01:39 -0700

    add new alggo

--- exercises/LC104_maxDepth/index.js
+++ exercises/LC104_maxDepth/index.js
@@ -1,3 +1,15 @@
-function maxDepth(root) {}
+/* Given the root of a binary tree, invert the tree, and return its root. */
+
+// Link: https://leetcode.com/problems/invert-binary-tree/
+
+// Time Complexity: O(n)
+
+// Space Complexity: O(n)
+
+var maxDepth = function(root) {
+    if(!root) return null;
+    let max = Math.max(maxDepth(root.left), maxDepth(root.right));
+    return max + 1;
+}
 
 module.exports = maxDepth;
