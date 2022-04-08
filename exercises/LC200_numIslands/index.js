eae629e4a4b1a3ef71bdcf3238b04cbf8c3b27b9
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-08 13:57:58 -0700

    add new algo

--- exercises/LC200_numIslands/index.js
+++ exercises/LC200_numIslands/index.js
@@ -1,13 +1,49 @@
-/* Given the head of a singly linked list, reverse the list, and return the reversed list. */
+/* Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
 
-// Link: https://leetcode.com/problems/merge-two-sorted-lists/
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water. */
 
-// Solution 1: While Loop
+// Link: https://leetcode.com/problems/number-of-islands/
 
-// Time Complexity: O(N)
+// Time Complexity: O(n2)
 
 // Space Complexity: O(1)
 
-function numIslands(grid) {}
+// DFS solution
+var numIslands = function(grid) {
+    const gridLength = grid.length
+    const rowLength = grid[0].length
+    let count = 0
+
+    // Loop through every cell in the grid
+    for(let i = 0; i < gridLength; i++){
+        for(let j = 0; j < rowLength; j++) {
+            // When we hit land we know we are on an island
+            // We will use this cell to visit all the 1's connected to it
+            // Once we've visited all the 1's connected to this cell then we increase the count
+            if(grid[i][j]==="1") {
+                dfs(i, j)
+                count++
+            }
+        }
+    }
+
+    // DFS search
+    // Look in all directions and set the current cell to 0 to prevent this cell from being visited again
+    function dfs(i,j) {
+        if(grid[i][j] ==="1") {
+            grid[i][j] = '0'
+            // look north
+            if(i - 1 >= 0) dfs(i-1,j)
+            // look east
+            if(j + 1 < rowLength) dfs(i,j+1)
+            // look south
+            if(i + 1 < gridLength) dfs(i+1,j)
+            // look west
+            if(j - 1 >= 0) dfs(i,j-1)
+        }
+    }
+
+    return count
+};
 
 module.exports = numIslands;
