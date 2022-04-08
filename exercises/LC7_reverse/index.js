2b2304e6cfb26fc875775358338d50d46c6cd8bc
Gino Tasis <gtasis84@gmail.com>
2022-04-08 18:28:18 -0400

    Add reverse num solution

--- exercises/LC7_reverse/index.js
+++ exercises/LC7_reverse/index.js
@@ -7,6 +7,26 @@
 // reverse(2147483649) --> 0
 // reverse(-2147483649) --> 0
 
-function reverse(x) {}
+function reverse(x) {
+
+    const limit = Math.pow(2, 31)
+    const n = Number(String(Math.abs(x)).split('').reverse().join(''));
+    
+    return n > limit ? 0 : n * Math.sign(x)
+}
+
+// solution without turning number into String
+
+function reverseNum(x) {
+
+    let reverse = 0;
+
+    while(x > 0) {
+        reverse = reverse * 10 + x % 10;
+        x = Math.floor(x/10)
+    }
+    
+    return x
+}
 
 module.exports = reverse;
