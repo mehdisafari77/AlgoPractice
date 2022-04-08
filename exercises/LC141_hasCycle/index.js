ede58b1c544c54347412503c64b27e6ccf14478a
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-08 13:49:51 -0700

    new algo added

--- exercises/LC141_hasCycle/index.js
+++ exercises/LC141_hasCycle/index.js
@@ -1,3 +1,11 @@
+/* Given the head of a singly linked list, reverse the list, and return the reversed list. */
+
+// Link: https://leetcode.com/problems/reverse-linked-list/
+
+// Time Complexity: O(N)
+
+// Space Complexity: O(1)
+
 function hasCycle(head) {}
 
 module.exports = hasCycle;
--- exercises/LC206_reverseList/index.js
+++ exercises/LC206_reverseList/index.js
@@ -1,3 +1,43 @@
-const reverseList = head => {};
+/* Given the head of a singly linked list, reverse the list, and return the reversed list. */
+
+// Link: https://leetcode.com/problems/reverse-linked-list/
+
+// Time Complexity: O(N)
+
+// Space Complexity: O(1)
+
+// recursive
+var reverseList = function(head) {
+    if (head === null) return null;
+    if (head.next === null) return head;
+    
+    var newHead = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    
+    return newHead;
+    
+};
+// non-recursive
+var reverseList = function(head) {
+    if (head === null) return null;
+    if (head.next === null) return head;
+    
+    
+    
+    var prev = head;
+    var cur = head.next;
+      
+    prev.next = null;
+    while (cur !== null) {
+        var next = cur.next;
+        cur.next = prev;
+        prev = cur;
+        cur = next;
+    }
+    
+    return prev;
+    
+};
 
 module.exports = reverseList;
