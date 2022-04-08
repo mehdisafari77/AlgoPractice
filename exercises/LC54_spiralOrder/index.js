e9ee740d3cdb9e69b0ad67355ec2bd1ec0fe950f
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-08 13:39:13 -0700

    new algo added

--- exercises/LC54_spiralOrder/index.js
+++ exercises/LC54_spiralOrder/index.js
@@ -1,3 +1,32 @@
-function spiralOrder(matrix) {}
+/* Given an m x n matrix, return all elements of the matrix in spiral order. */
 
+// Link: https://leetcode.com/problems/spiral-matrix/
+
+// Time Complexity: O(n)
+
+// Space Complexity: O(n)
+
+function spiralOrder(matrix) {
+    var n = matrix.length;
+    var m = (matrix[0] || []).length;
+    var res = [];
+    var x1 = 0;
+    var x2 = m - 1;
+    var y1 = 0;
+    var y2 = n - 1;
+
+    while (x1 <= x2 && y1 <= y2) {
+      for (var x = x1; x <= x2; x++) res.push(matrix[y1][x]);
+      for (var y = y1 + 1; y <= y2; y++) res.push(matrix[y][x2]);
+      if (x1 < x2 && y1 < y2) {
+        for (var x = x2 - 1; x > x1; x--) res.push(matrix[y2][x]);
+        for (var y = y2; y > y1; y--) res.push(matrix[y][x1]);
+      }
+      x1++;
+      x2--;
+      y1++;
+      y2--;
+    }
+    return res;
+  };
 module.exports = spiralOrder;
