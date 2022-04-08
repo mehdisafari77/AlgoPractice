eb04b1b972fad90474645c8041630c5fbbb60edd
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-08 13:50:40 -0700

    add new algo

--- exercises/LC141_hasCycle/index.js
+++ exercises/LC141_hasCycle/index.js
@@ -6,6 +6,15 @@
 
 // Space Complexity: O(1)
 
-function hasCycle(head) {}
+var hasCycle = function(head) {
+    var slow = head;
+    var fast = head;
+    while (slow && fast) {
+      slow = slow.next;
+      fast = fast.next ? fast.next.next : undefined;
+      if (slow === fast) return true;
+    }
+    return false;
+  };
 
 module.exports = hasCycle;
