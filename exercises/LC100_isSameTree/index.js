82746df27148ddf0ad4a532259d2e6df36fe70c1
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-08 14:10:23 -0700

    add new alggo

--- exercises/LC100_isSameTree/index.js
+++ exercises/LC100_isSameTree/index.js
@@ -1,3 +1,25 @@
-function isSameTree(p, q) {}
+/* Given two binary trees, write a function to check whether they are the same.
+
+Two trees are considered identical if they are structurally identical and nodes have the same value. */
+
+// Link: https://leetcode.com/problems/same-tree/
+
+// Time Complexity: O(n)
+
+// Space Complexity: O(n)
+
+var isSameTree = function(p, q) {
+    if (p === null && q === null) {
+    return true;
+  } else if (p === null || q === null) {
+    return false;
+  }
+  if (p.val === q.val) {
+    let leftSame = isSameTree(p.left, q.left);
+    let rightSame = isSameTree(p.right, q.right);
+    return leftSame && rightSame;
+  }
+  return false;
+};
 
 module.exports = isSameTree;
