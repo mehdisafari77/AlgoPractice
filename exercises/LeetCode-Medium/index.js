3eee6816113f951e0ce89d17929a671fb4c1bb7e
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-13 19:09:04 -0700

    add new algo

---
+++ exercises/LeetCode-Medium/index.js
@@ -0,0 +1,28 @@
+/* Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
+
+Return the sorted string. If there are multiple answers, return any of them. */
+
+// Link: https://leetcode.com/problems/sort-characters-by-frequency/
+
+// Time Complexity: O(n)
+
+// Space Complexity: O(n)
+
+var frequencySort = function(s){
+    let map = {};
+    
+    for(const letter of s){
+      map[letter] = (map[letter] || 0) + 1;
+      
+    }
+    let result = "";
+    let sorted = Object.keys(map).sort((a,b)=> map[b] - map[a]);
+    for(letter of sorted){
+      for(let i = 0; i < map[letter]; i++){
+        result += letter;
+      }
+    }
+    return result;
+  }
+  let string = "tree";
+  console.log(frequencySort(string));
