70fc6f0c6ec7ec14f79a533370a4a3de2811d42f
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-14 13:23:21 -0700

    minor edits in algo

--- exercises/LeetCode-Medium/LC6_zigzagConversion/index.js
+++ exercises/LeetCode-Medium/LC6_zigzagConversion/index.js
@@ -1,3 +1,20 @@
+/* The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows); */
+
+// Link: https://leetcode.com/problems/zigzag-conversion/
+
+// Time Complexity: 
+
+// Space Complexity: 
+
 var convert = function (inputString, targetRows) {
     if (targetRows == 1) {
         return inputString;
