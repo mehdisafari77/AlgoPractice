745be82f6f40909d66dd0c666009ac250bb33de8
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-05-04 15:46:39 -0700

    add new algo

--- exercises/LeetCode-Medium/LC11_maxArea/index.js
+++ exercises/LeetCode-Medium/LC11_maxArea/index.js
@@ -22,3 +22,25 @@ var maxArea = function(H) {
     return ans
 };
 
+// Solution 2
+var maxArea = function(height) {
+    // Max amountof water
+    let max = 0
+    // Pointer to move possible heights
+    let left = 0
+    let right = height.length-1
+    // Use 2 pointers to find the max area
+    while(left < right) {
+        // Current area
+        const area = Math.min(height[left], height[right]) * (right - left)
+        // Check if current area is greater than previous max
+        max = Math.max(max, area)
+        // Move the pointer that has the lowest height
+        if(height[left]>height[right]) {
+            right--
+        } else {
+            left++
+        }
+    }
+    return max
+}
