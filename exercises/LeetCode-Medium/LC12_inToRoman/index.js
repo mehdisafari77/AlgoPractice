aaa42c322673b0a6426754779e0ea35ffc4cccb2
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-05-05 19:49:49 -0700

    add 2 more solutions to algo

--- exercises/LeetCode-Medium/LC12_inToRoman/index.js
+++ exercises/LeetCode-Medium/LC12_inToRoman/index.js
@@ -40,3 +40,40 @@ var intToRoman = function (num) {
     if (num >= 1) return 'I' + intToRoman(num - 1);
     return num;
 };
+
+// Solution 2
+var intToRoman = function (num) {
+    const list = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
+    const valueList = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    let result = '';
+
+    // Run until we have converted the full number
+    while (num !== 0) {
+        // Loop though the available numerals
+        for (let i = 0; i < list.length; i++) {
+            // Check if the outstanding number is greater than the current numeral
+            if (num >= valueList[i]) {
+                // If so, add this numeral to the result and subtract its value from the outstanding number
+                result += list[i];
+                num -= valueList[i];
+                break;
+            }
+        }
+    }
+    return result;
+};
+
+// Solution 3
+var intToRoman = function (num) {
+    const M = ['', 'M', 'MM', 'MMM'];
+    const C = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'];
+    const X = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'];
+    const I = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+
+    return (
+        M[Math.floor(num / 1000)] +
+        C[Math.floor((num % 1000) / 100)] +
+        X[Math.floor((num % 100) / 10)] +
+        I[num % 10]
+    );
+};
