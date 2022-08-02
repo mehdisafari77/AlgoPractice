d5772498715466568d5e18943ab2bac740eba2c0
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-08-02 09:57:28 -0700

    add algo solution for leetcode 22

--- exercises/LeetCode-Medium/LC22_generateParantheses/index.js
+++ exercises/LeetCode-Medium/LC22_generateParantheses/index.js
@@ -14,3 +14,46 @@ Output: ["()"] */
 
 // Solution:
 
+var generateParenthesis = function (n) {
+    let optionsArray = [];
+
+    function innerGenerateParentheses(
+        currentString,
+        openBrackets,
+        closedBrackets,
+        maximumPairs
+    ) {
+        // If the current string is full
+        if (currentString.length == maximumPairs * 2) {
+            // Add it to the list of possibilities
+            optionsArray.push(currentString);
+            return;
+        }
+
+        // If more brackets can be opened
+        if (openBrackets < maximumPairs) {
+            // Call the function again, with one additional bracket opened
+            innerGenerateParentheses(
+                currentString + '(',
+                openBrackets + 1,
+                closedBrackets,
+                maximumPairs
+            );
+        }
+
+        // If open brackets still need to be closed
+        if (closedBrackets < openBrackets) {
+            // Call the function again, with one additional bracket closed
+            innerGenerateParentheses(
+                currentString + ')',
+                openBrackets,
+                closedBrackets + 1,
+                maximumPairs
+            );
+        }
+    }
+
+    // Recursively obtain all possibilties
+    innerGenerateParentheses('', 0, 0, n);
+    return optionsArray;
+};
