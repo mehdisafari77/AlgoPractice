d2c4f902f04518dc2c99d6d8206fc441bd4e5db6
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-08-14 15:49:54 -0700

    done wth algo 28 leetcode

--- exercises/LeetCode-Easy/LC28_implementstrStr/index.js
+++ exercises/LeetCode-Easy/LC28_implementstrStr/index.js
@@ -20,3 +20,17 @@ Input: haystack = "aaaaa", needle = "bba"
 Output: -1 */
 
 // Link: https://leetcode.com/problems/implement-strstr/
+
+var strStr = function (haystack, needle) {
+    if (needle.length === 0) return 0;
+  
+    if (needle === haystack) return 0;
+  
+    for (let i = 0; i <= haystack.length - needle.length; i++) {
+      if (needle === haystack.substring(i, i + needle.length)) {
+        return i;
+      }
+    }
+  
+    return -1;
+  };
